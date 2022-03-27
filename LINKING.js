e0a1d6250b25807caca0ec8ddd4df88bbf22464a
Doorstep <81813325+Doorstep7286@users.noreply.github.com>
2022-03-27 18:27:37 +0100

    Add files via upload

---
+++ LINKING.js
@@ -0,0 +1,18 @@
+function discord(url) {
+    window.open('https://discord.gg/WayU6WSTW7', '_blank');
+    }
+function Movies(url) {
+    window.open('Movies.html');
+    }
+function Tvshows(url) {
+    window.open('Tv Shows.html', '_blank');
+    }
+function Anime(url) {
+    window.open('Anime.html');
+    }
+function info(url) {
+        window.open('Info.html', '_blank');
+        }
+function home(url) {
+        window.open('index.html');
+        }
